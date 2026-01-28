761ade51e79e90a204eac6768d5933b390bd0a33
Potey Charles <ppsjcfullstackdeveloper@gmail.com>
2026-01-28 00:34:52 +0000

    Realisation de la fonctionnalite partage de score

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-const { use } = require("react");
-
 function displayScore(score, totalScore){
     let scoreArea = document.querySelector(".score-area span");
     scoreArea.innerHTML = `${score} / ${totalScore}`;
@@ -9,6 +7,24 @@ function displayScore(score, totalScore){
 function displayProposition(proposition){
     let propositionArea = document.querySelector(".proposition-area");
     propositionArea.innerHTML = proposition;
+}
+
+function emailVerification(email){
+    let regex = new RegExp("[a-zA-Z0-9.-_]+@[a-zA-Z0-9.-_]+\\.[a-zA-Z0-9.-_]+");
+
+    let result = regex.test(email);
+    if(result){
+        return true;
+    }
+     else{
+        return false;
+     }
+}
+
+function sendScore(name, email, score){
+    let mailto = `mailto:${email}?Subject=Partage de score&body=Bonjour je me nomme ${name} et je voudrais partager le score ${score} que j'ai obtenue a la fin du jeu`;
+
+    location.href = mailto;
 }
 
 
@@ -66,6 +82,37 @@ function main(){
             validateButton.disabled = true;
             shareButton.style.display = "flex";
         }
+    })
+
+    //opening and closing dialog...
+    const closeButton = document.getElementById("btn-close");
+    const modal = document.querySelector("dialog");
+
+    shareButton.addEventListener("click", () => {
+        modal.showModal();
+    });
+
+    closeButton.addEventListener("click", () => {
+        modal.close();
+    });
+
+    //form script...
+    const form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+        //we prevent default
+        event.preventDefault();
+
+        const userName = document.getElementById("name").value;
+        const userEmail = document.getElementById("email").value;
+        const userScore = `${score} / ${totalScore}`;
+
+
+        if(emailVerification(userEmail)){
+            sendScore(userName, userEmail, userScore);
+        }
+         else{
+            alert("L'email est invalid");
+         }
     })
 
     //we display score...
